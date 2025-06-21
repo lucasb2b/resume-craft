7aa313b73d212ffe8d3c22f24532b095ffa24a52
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-06-20 23:47:23 -0300

    Finalizando o Layout e adicionando Dark Mode

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,8 @@
 import { ReactNode } from "react"
 import Logo from "@/assets/logo.svg"
+import { NavItems } from "@/components/pages/dashboard/nav-items";
+import { UserDropDown } from "@/components/pages/dashboard/user-dropdown";
+import { ThemeToggle } from "@/components/shared/theme-toggle";
 
 type DashboardLayoutProps = {
   children: ReactNode;
@@ -12,9 +15,16 @@ export default function DashboardLayout({ children }: DashboardLayoutProps){
         <div className="w-full p-6 boder-b border-muted">
           <Logo className="max-w-[100px] mx-auto" />
         </div>
+
+        <NavItems />
+
+        <div className="w-full mt-auto border-t border-muted px-3 py-4 flex items-center justify-between gap-2">
+          <UserDropDown/>
+          <ThemeToggle />
+        </div>
       </aside>
 
-      <main>
+      <main className="p-6 flex flex-col w-full h-full overflow-auto">
         {children}
       </main>
     </div>
